023059d8dc6311809e2c5b2e1ca32ddc6b5ca25c
Your Name <you@example.com>
2018-05-02 14:06:53 -0600

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { ProductService } from './shared/services/product.service';
+
+@Component({
+  selector: 'body',
+  template:`<router-outlet></router-outlet>`
+})
+export class AppComponent {
+
+  constructor() {
+
+    
+  }
+ 
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { ProductService } from './shared/services/product.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing';
+import { MasterPageComponent } from './shared/masterpage/master.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MasterPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase, 'ngproductreview'),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    FormsModule
+  ],
+  providers: [
+    ProductService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
